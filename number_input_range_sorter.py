75c78c4fc9c88fb284fd5194469c49530c517157
RonAndreMalabuyoc <rondremalabuyoc@gmail.com>
2024-10-30 15:27:45 +0800

    managed to remove the error, and removed the psuedocode that was confusing me

--- number_input_range_sorter.py
+++ number_input_range_sorter.py
@@ -1,19 +1,9 @@
-#set an array to store the input values to be checked later
-#setup a input system for the numbers being input
-#Check the input numbers if it goes over the range
-#Check how many of the numbers goes into a set in a range of 10s
-
-
-#main variable
 user_input = {}
-#loop
+
 while True:
     try:
-#input numbers
         numbers = int(input("Please input a number from 1-50, if not there will be consequences"))
-    #if it isn't an integer, or number goes over 50, print invalid
+
         Invalid = numbers < 50 in user_input
     except:
-#end loop
-
-#find the values inputted and sort them in ranges of 10s
+        print

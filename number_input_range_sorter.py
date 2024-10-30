6ea1c9ae30680b05ce93a74ccba425bfc41eba3c
RonAndreMalabuyoc <rondremalabuyoc@gmail.com>
2024-10-30 13:35:21 +0800

    just psuedocode

---
+++ number_input_range_sorter.py
@@ -0,0 +1,15 @@
+#set an array to store the input values to be checked later
+#setup a input system for the numbers being input
+#Check the input numbers if it goes over the range
+#Check how many of the numbers goes into a set in a range of 10s
+
+
+#main variable
+#loop
+#input numbers
+    #if it isn't an integer, or number goes over 50, print invalid
+
+#end loop
+
+#find the values inputted and sort them in ranges of 10s
+

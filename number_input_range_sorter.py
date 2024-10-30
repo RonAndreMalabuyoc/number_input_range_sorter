ee9c500e29fd2dfa8b4e77225aaf05f31efb2709
RonAndreMalabuyoc <rondremalabuyoc@gmail.com>
2024-10-30 15:33:09 +0800

    got it to break if the input is over 50

--- number_input_range_sorter.py
+++ number_input_range_sorter.py
@@ -2,8 +2,10 @@ user_input = {}
 
 while True:
     try:
-        numbers = int(input("Please input a number from 1-50, if not there will be consequences"))
+        numbers = int(input("Please input a number from 1-50, if not there will be consequences: "))
 
         Invalid = numbers < 50 in user_input
+        if numbers > 51:
+            break
     except:
-        print
+        print("Whoops, wrong")

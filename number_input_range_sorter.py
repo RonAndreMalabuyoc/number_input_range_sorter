b31e62a69fa568a8f605394c7a095ed568b428f4
RonAndreMalabuyoc <rondremalabuyoc@gmail.com>
2024-10-30 14:33:04 +0800

    I keep recieving an unexpected indented block error, otherwise, good on the input and invalid lines

--- number_input_range_sorter.py
+++ number_input_range_sorter.py
@@ -5,11 +5,15 @@
 
 
 #main variable
+user_input = {}
 #loop
+while True:
+    try:
 #input numbers
+        numbers = int(input("Please input a number from 1-50, if not there will be consequences"))
     #if it isn't an integer, or number goes over 50, print invalid
-
+        Invalid = numbers < 50 in user_input
+    except:
 #end loop
 
 #find the values inputted and sort them in ranges of 10s
-

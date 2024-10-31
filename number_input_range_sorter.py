df63eaf2fec5c7e8d97ab1b2a7ecaeb57158c725
RonAndreMalabuyoc <rondremalabuyoc@gmail.com>
2024-10-31 17:20:33 +0800

    Trying appending method to count the amount of numbers in a set, not currently working

--- number_input_range_sorter.py
+++ number_input_range_sorter.py
@@ -15,3 +15,27 @@ while True:
             break
     except:
         print("welp")
+
+row_10 = []
+row_20 = []
+row_30 = []
+row_40 = []
+row_50 = []
+
+if numbers <= 50:
+    row_50.append(numbers)
+    count_50 = len(row_50)
+elif numbers <= 40:
+    row_40.append(numbers)
+    count_40 = len(row_40)
+elif numbers <= 30:
+    row_30.append(numbers)
+    count_30 = len(row_30)
+elif numbers <= 20:
+    row_20.append(numbers)
+    count_20 = len(row_20)
+elif numbers <= 10:
+    row_10.append(numbers)
+    count_10 = len(row_10)
+
+print(row_50, row_40, row_30, row_20, row_10)
